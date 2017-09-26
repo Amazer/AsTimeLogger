8c0e0d28066786e4f748b94c68fc46e32ee6244a
Amazer <149779511@qq.com>
2017-09-26 20:12:33 +0800

    change layout

--- app/src/main/java/timelogger/cyc/astimelogger/DateListView.java
+++ app/src/main/java/timelogger/cyc/astimelogger/DateListView.java
@@ -1,18 +1,42 @@
 package timelogger.cyc.astimelogger;
 
-import android.app.ListActivity;
-import android.widget.TextView;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ListView;
 
 /**
  * Created by cyc on 2017/9/23.
  */
 
-public class DateListView extends ListActivity {
-    public  final class ViewHolder
-    {
-        public TextView v0;
-        public TextView v1;
-        public TextView v2;
-        public TextView v3;
+public class DateListView extends ListView {
+    // region  Constructor
+    public DateListView(Context context) {
+        super(context);
     }
+
+    public DateListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DateListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public DateListView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    public void addHeaderView(View v, Object data, boolean isSelectable) {
+        super.addHeaderView(v, data, isSelectable);
+    }
+
+    @Override
+    public void setDivider(@Nullable Drawable divider) {
+        super.setDivider(divider);
+    }
+    // endregion
 }
