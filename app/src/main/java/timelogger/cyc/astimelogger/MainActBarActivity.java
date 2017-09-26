b10a0f3c22f5d23ecc5b48e4b355c71a3fb69208
Amazer <149779511@qq.com>
2017-09-26 19:14:52 +0800

    recyclerView need android sdk 25

--- app/src/main/java/timelogger/cyc/astimelogger/MainActBarActivity.java
+++ app/src/main/java/timelogger/cyc/astimelogger/MainActBarActivity.java
@@ -1,7 +1,6 @@
 package timelogger.cyc.astimelogger;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -11,8 +10,9 @@ import android.support.v7.app.AppCompatActivity;
 public class MainActBarActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_2);
+        // recyclerView need android skd 25
     }
 }
