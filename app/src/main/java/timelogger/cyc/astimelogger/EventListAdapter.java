12dce15bf5b84fe5ac50bbb09cae6f9281a15c15
Amazer <149779511@qq.com>
2017-10-17 14:01:05 +0800

    event list

--- app/src/main/java/timelogger/cyc/astimelogger/EventListAdapter.java
+++ app/src/main/java/timelogger/cyc/astimelogger/EventListAdapter.java
@@ -13,40 +13,69 @@ import java.util.ArrayList;
  * Created by 95 on 2017/9/25.
  */
 
-public class EventListAdapter extends BaseAdapter {
+public class EventListAdapter extends BaseAdapter
+{
 
     private ArrayList<String> _dataList;
     private LayoutInflater _inflater;
 
-    public EventListAdapter(Context context, ArrayList<String> d) {
+    public EventListAdapter(Context context, ArrayList<String> d)
+    {
         _dataList = d;
         _inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
-    public int getCount() {
-        return _dataList.size();
+    public int getCount()
+    {
+        return _dataList.size() + 1;
     }
 
     @Override
-    public Object getItem(int position) {
+    public Object getItem(int position)
+    {
         return position;
     }
 
     @Override
-    public long getItemId(int position) {
+    public long getItemId(int position)
+    {
         return position;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View vi=convertView;
-        if (vi == null) {
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        EventViewHolder holder;
+        View vi = convertView;
+        if (vi == null)
+        {
             vi = _inflater.inflate(R.layout.eventlist_item, null);
+            holder = new EventViewHolder();
+            holder.tagBtn = (Button) vi.findViewById(R.id.tag_button);
+            holder.managerBtn = (Button) vi.findViewById(R.id.manager_button);
+            vi.setTag(holder);
+        } else
+        {
+            holder = (EventViewHolder) vi.getTag();
         }
-        String s=_dataList.get(position);
-        Button btn=(Button)vi.findViewById(R.id.button);
-        btn.setText(s);
+
+        if (holder != null)
+        {
+            holder.position = position;
+            if(position>=(getCount()-1))
+            {
+                holder.tagBtn.setVisibility(View.INVISIBLE);
+                holder.managerBtn.setVisibility(View.VISIBLE);
+            }
+            else
+            {
+                String s = _dataList.get(position);
+                holder.tagBtn.setTag(s);
+                holder.tagBtn.setVisibility(View.VISIBLE);
+                holder.managerBtn.setVisibility(View.INVISIBLE);
+            }
+        }
         return vi;
     }
 }
---
+++ app/src/main/java/timelogger/cyc/astimelogger/EventViewHolder.java
@@ -0,0 +1,14 @@
+package timelogger.cyc.astimelogger;
+
+import android.widget.Button;
+
+/**
+ * Created by cyc on 2017/10/17.
+ */
+
+public class EventViewHolder
+{
+    public Button tagBtn;
+    public Button managerBtn;
+    public int position;
+}
